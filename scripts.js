0727f14a0926f4626336285b8e6b99c616d8294c
Gerard Oosterhof <gerard@script.nl>
2021-06-29 09:51:22 +0200

    Code Review

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ const myName = 'Peter Riksten';
 const nameColor = ["red", "blue", "black", "green", "orange"];
 
 function changeName(){
+    // TODO :: use more descriptive variable names
+    // TODO :: it's better to use nameColor.length instead of a hardcoded 5
+    // because that way when you add a new color it wel be chosen randomly as well
     let a = Math.floor(Math.random()*5);
     let b = nameColor[a];
     document.getElementById("targetText").innerHTML = "Hi " + myName + "!";
